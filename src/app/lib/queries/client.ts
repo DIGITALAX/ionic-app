5d4ea07eb3b469a777313cf07f4439d257044721
emmajane1313 <emma.jane@digitalax.xyz>
2025-11-13 00:55:59 -0500

    node

--- src/app/lib/queries/client.ts
+++ src/app/lib/queries/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 
 const httpLink = new HttpLink({
-  uri: `https://gateway.thegraph.com/api/${process.env.NEXT_PUBLIC_GRAPH_KEY}/subgraphs/id/5L34okSHnRJBuGjba9R7mSERdcJZY9vA2YFDW2iF6fmr`,
+  uri: "/api/graphql/ionic",
 });
 
 export const graphClient = new ApolloClient({
